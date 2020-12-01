5201218f3a21d3cf203eac2de6f26f13988b0cb5
Rojre Byfield <44046579+Rolo88@users.noreply.github.com>
2020-12-01 03:56:20 -0500

    Add files via upload

---
+++ roundrob.js
@@ -0,0 +1,13 @@
+var c1=document.getElementsByClassName("c1");
+var c2=document.getElementsByClassName("c2");
+var c3=document.getElementsByClassName("c3");
+
+var pos=0;
+
+function first(){
+
+    }
+
+function second(){
+
+}
