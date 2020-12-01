b5c15cc73d66f4b035986ecdab3768afd346c9df
hollowayjenelle <hollowayjenelle@gmail.com>
2020-12-01 10:24:07 -0500

    updated fcfs.js

--- fcfs.js
+++ fcfs.js
@@ -1,24 +1,24 @@
 function addprocess(){
     var table = document.getElementById("process-table");
 
-    var lastRow = table.rows[table.rows.length-1];
-    var firstCell = lastRow.cells[0].innerHTML;
-    var lastRowNumber, arrival;
+    var lastRow = table.rows[table.rows.length-1]; //gets the contents of the last row
+    var firstCell = lastRow.cells[0].innerHTML; //gets the content of the first cell in the last row
+    var lastRowNumber;
     if (table.rows.length <= 1){
         lastRowNumber = -1;
-	arrival = -1;
     } else{
-        lastRowNumber = parseInt(firstCell[1]);
-	arrival = parseInt(firstCell[1]);
+        lastRowNumber = parseInt(firstCell[1]); //gets the number from the first cell of the last row
     }
     
-
+    //inserts a new row and new cells
+    var row = table.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
-
+    
+    //add content to the new cells
     cell1.innerHTML = 'P'+ (lastRowNumber + 1);
-    cell2.innerHTML = arrival + 1;
+    cell2.innerHTML = lastRowNumber + 1;
     cell3.innerHTML = '<input class="executime" type="text" value=""/>';
 
 }
