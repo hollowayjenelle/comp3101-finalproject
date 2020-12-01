a34f8e836cf2967ff0e4970af1f56dd26e54db30
hollowayjenelle <hollowayjenelle@gmail.com>
2020-12-01 05:19:47 -0500

    updated files + round robin added

--- roundrobin.js
+++ roundrobin.js
@@ -7,17 +7,21 @@ function addprocess(){
     if (table.rows.length <= 1){
         lastRowNumber = -1;
     } else{
-        lastRowNumber = parseInt(firstCell[1]); //gets the number from the first cell of the last row
+        //gets the number from the first cell of the last row
+        lastRowNumber = parseInt(firstCell[1]);
+        arrival = parseInt(firstCell[1]);
     }
     
     //inserts a new row and new cells
     var row = table.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    
+    var cell3 = row.insertCell(2);
+
     //add content to the new cells
     cell1.innerHTML = 'P'+ (lastRowNumber + 1);
-    cell2.innerHTML = '<input class="executime" type="text" value=""/>';
+    cell2.innerHTML = arrival + 1;
+    cell3.innerHTML = '<input class="executime" type="text" value=""/>';
 
 }
 
@@ -34,27 +38,31 @@ function draw(){
     var processtable = $('#process-table tr');
     var th = '';
 
+    var quantum = $('#quantum').val();
     var executeTimes = [];
 
     //for each row, the execution time and the process # is extracted
     $.each(processtable, function(key, value){
-        if (key == 0) return true;
-        var executeTime = parseInt($(value.children[1]).children().first().val());
-        executeTimes[key - 1] = {"executeTime": executeTime, "P": key -1};
+        if(key == 0) return true;
+        var executeTime = parseInt($(value.children[2]).children().first().val());
+        executeTimes[key - 1] = {"executeTime": executeTime, "P": key - 1};
     });
 
-    //compare and sort the execution times
-    executeTimes.sort(function (a, b){
-        if (a.executeTime == b.executeTime){
-            return a.P - b.P
-        }
-        return a.executeTime - b.executeTime
-    });
-
-    //for each value in the sorted execution time array, a table header with a button is created with the process and the execution time
-    $.each(executeTimes, function(key, value){
-        th += '<th width: 50px;><button class="btn2">P' + value.P + '<br>Ex Time: ' + value.executeTime + 'ms</button></th>';
-    });
+    var complete = false; //variable to keep track of the execution time for the process has reached 0
+    while (!complete){
+        complete = true;
+        /*for every value in executeTimes, if the current executeTime is greater than 
+        0, the quantum value is displayed if the execute time is greater than quantum. If not, 
+        the execute time is displayed.
+        */
+        $.each(executeTimes, function(key,value){
+            if(value.executeTime > 0){
+                th += '<th width: 50px;><button class="btn2">P' + value.P + '<br>Ex Time: ' + (value.executeTime > quantum ? quantum : value.executeTime) + 'ms</button></th>';
+                value.executeTime -= quantum;
+                complete = false;
+            }
+        });
+    }
         //table created within the section element
         $('section').html('<table id="resultTable"><tr>' + th + '</tr></table>');
         //div created that surrounds the section element 
@@ -72,7 +80,6 @@ function draw(){
         
         var distance = $("#animated-div").css("width");
         
-        //swipe animation
         jQuery('#animated-div').animate({ width: '0', marginLeft: distance}, sum*1000/2, 'linear');
 }
 
